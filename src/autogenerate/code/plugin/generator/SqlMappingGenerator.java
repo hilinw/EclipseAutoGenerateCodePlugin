5f018b026029b7c56a0272e6d78e5a35a8a2431c
hilinw <441089@qq.com>
2021-02-23 14:05:07 +0800

    wlh提交 增加使用like查询的字段

--- src/autogenerate/code/plugin/generator/SqlMappingGenerator.java
+++ src/autogenerate/code/plugin/generator/SqlMappingGenerator.java
@@ -477,16 +477,31 @@ public class SqlMappingGenerator {
 			sb.append("and ");
 			sb.append(column.name());
 			
-			sb.append(" = #{");
-			sb.append(field.getName());
-			sb.append("}");
-			
+			if(isLikeField(field.getName()) ) {
+				sb.append(" like '%${");
+				sb.append(field.getName());
+				sb.append("}%");
+			}else {
+				sb.append(" = #{");
+				sb.append(field.getName());
+				sb.append("}");
+			}
 			sb.append("\n\t\t\t");			
 			sb.append("</if>");
 			
 		}
 		return sb.toString();
 
+	}
+	/**
+	 * 使用like查询的字段
+	 */
+	private boolean isLikeField(String fileName) {
+		if("name".equalsIgnoreCase(fileName) || "number".equalsIgnoreCase(fileName)
+				|| "remark".equalsIgnoreCase(fileName)) {
+			return true;
+		}
+		return false;
 	}
 
 	private String getAddFields() {
